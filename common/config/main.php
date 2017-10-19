000b8326425a270993ff1194617e76bf37117321
manbanpai <761846158@qq.com>
2017-10-19 14:04:42 +0800

    main

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+    	'db' => [
+    		'class' => 'yii\db\Connection',
+    		'dsn' => 'mysql:host=yz-pub-atlas-mysql.glodon.org;port=4022;dbname=wangcaiv3',
+    		'username' => 'db_zfxc',
+    		'password' => 'db_zfxc',
+    		'charset' => 'utf8',
+    	],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
