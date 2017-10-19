7a917f9865f7e8c4eaeb0e31ac0bea2cba9cdbbb
manbanpai <761846158@qq.com>
2017-10-19 15:34:49 +0800

    env

--- environments/index.php
+++ environments/index.php
@@ -51,15 +51,15 @@ return [
         'setWritable' => [
             'backend/runtime',
             'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'api/runtime',
+            'api/web/assets',
         ],
         'setExecutable' => [
             'yii',
         ],
         'setCookieValidationKey' => [
             'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'api/config/main-local.php',
         ],
     ],
 ];
